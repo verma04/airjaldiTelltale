64d42af2fe25508576913c6d698d03b1b0c1cf81
Pankaj <pankaj.k@amakein.com>
2021-06-18 12:19:16 +0530

    sdd

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -7,16 +7,5 @@ export const SET_ALERT = "SET_ALERT"
 export const NEWS = "NEWS"
 export const DRAFT = "DRAFT"
 export const REMOVE_ALERT  = "REMOVE_ALERT "
-export const PUB = "PUB"
-export const CARRER = "CARRER"
-export const ABOUT = "ABOUT";
-export const PROFILE = "PROFILE";
-export const SERVICES = "SERVICES";
-export const TRAIN = "TRAIN";
-export const STORY = "STORY";
-export const NET = "NET";
-export const PEOPLE = "PEOPLE";
-export const PRO= "PRO";
-export const MEMBER = "MEMBER";
-// Users
+
 
